e482956e3314bb901964e6ceb851d2105b3d17bd
Raul Victor Trombin <raulvtrombin@gmail.com>
2025-02-12 18:58:03 -0300

    blueos-ping-viewer-next: Dockerfile: Bind volume following standards

--- blueos-ping-viewer-next/files/entrypoint.sh
+++ blueos-ping-viewer-next/files/entrypoint.sh
@@ -2,4 +2,7 @@
 set -m
 echo "Starting ping viewer next..."
 cd app
+mkdir logs
+chmod -R 755 /app/logs
+chown -R pingviewer:pingviewer /app/logs
 su pingviewer -c "./ping-viewer-next --enable-auto-create --rest-server 0.0.0.0:6060"
